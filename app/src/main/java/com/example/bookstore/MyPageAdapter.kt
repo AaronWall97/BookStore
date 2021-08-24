26e3e50ae80cc34f52301227a9fddeee5e8c8515
AaronWall97 <aaron.wall@rocketmail.com>
2021-08-24 14:48:30 +0100

    Update

---
+++ app/src/main/java/com/example/bookstore/MyPageAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.bookstore
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MyPageAdapter (fm : FragmentManager) : FragmentPagerAdapter(fm){
+
+    val pageNumber = 3
+
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0 -> FragmentOne()
+            1 -> FragmentTwo()
+            3 -> FragmentThree()
+
+            else -> FragmentOne()
+        }
+    }
+
+    override fun getCount(): Int {
+        return pageNumber
+    }
+}
