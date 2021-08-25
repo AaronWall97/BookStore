febc16e5107a372d28bb078714eccf6466bceedf
AaronWall97 <aaron.wall@rocketmail.com>
2021-08-25 19:44:49 +0100

    Update

--- app/src/main/java/com/example/bookstore/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(findViewById(R.id.toolbar))
+
         viewPager = findViewById(R.id.viewPager)
         tabLayout = findViewById(R.id.tab_layout)
 
