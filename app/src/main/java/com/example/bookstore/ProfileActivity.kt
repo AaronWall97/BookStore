728a44d7c5722eedb5850d8b43b761366203c8cd
AaronWall97 <aaron.wall@rocketmail.com>
2021-08-26 17:10:36 +0100

    Update

--- app/src/main/java/com/example/bookstore/ProfileActivity.kt
+++ app/src/main/java/com/example/bookstore/ProfileActivity.kt
@@ -1,13 +1,34 @@
 package com.example.bookstore
 
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import de.hdodenhof.circleimageview.CircleImageView
+import android.content.SharedPreferences
+import android.util.Log
+import java.io.IOException
+
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var profileImage : CircleImageView
+
+    companion object {
+        private const val SELECT_IMAGE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+
+        profileImage = findViewById(R.id.circle_image_view)
+
 
         setSupportActionBar(findViewById(R.id.include))
 
@@ -18,8 +39,37 @@ class ProfileActivity : AppCompatActivity() {
             setDisplayShowHomeEnabled(true)
         }
 
+        profileImage.setOnClickListener{
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_IMAGE)
+        }
 
 
 
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK){
+                if(data != null){
+                    try{
+                        val selectedImage = data.data
+                        val yourSelectedImage = BitmapFactory.decodeStream(contentResolver.openInputStream(selectedImage!!))
+                        profileImage.setImageBitmap(yourSelectedImage)
+                    }catch(e : IOException){
+                        Log.e("ProfileActivity","The image is not loaded : $e")
+                    }
+
+                }
+            }
+        }
     }
 }
